87ff676bf6b974bbf72a83b0093c951ff28e0922
Liu Yi <326534215@qq.com>
2023-12-28 11:48:49 +0800

    fix: hidden api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export const getTableRecordsData = (tableId: string, viewId: string) => {
-  return axios.get(``, {
-    params: {
-      viewId: viewId,
-      fieldKeyType: 'id',
-      cellFormat: 'text',
-    },
-  });
-};
+// export const getTableRecordsData = (tableId: string, viewId: string) => {
+//   return axios.get(``, {
+//     params: {
+//       viewId: viewId,
+//       fieldKeyType: 'id',
+//       cellFormat: 'text',
+//     },
+//   });
+// };
 
-export const getTablefieldsData = (tableId: string, viewId: string) => {
-  return axios.get(``, {
-    params: {
-      viewId: viewId,
-    },
-  });
-};
+// export const getTablefieldsData = (tableId: string, viewId: string) => {
+//   return axios.get(``, {
+//     params: {
+//       viewId: viewId,
+//     },
+//   });
+// };
